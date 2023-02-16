dfe99de974db933ef81ca1688bcc1fef83315801
e <e>
2023-02-16 03:25:25 +0200

    test

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const nume = 'Geoka'
+
+document.querySelector('h1').innerText=nume
+
 document.querySelector("h2").style.display = "none"
 
 document.querySelector("button").addEventListener("click", () => {
